e30cbfd54821e1d8a944977d76fe67366bfc065c
Amya Moore <bookworm7572@gmail.com>
2025-05-19 12:43:21 -0400

    re-init commit

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-// https://astro.build/config
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
-export default defineConfig({
-	output: "static",
-	adapter: vercel(),
-});
